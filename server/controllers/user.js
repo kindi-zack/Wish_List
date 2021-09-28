03892c23ae9c5a36a04f5586cea56be7fc04d6d1
kindi-zack <alkindi.ibn.zakaria@mail.com>
2021-09-28 14:46:21 +0700

    server login

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,34 @@
 const { User } = require('../models')
+const { comparePassword } = require('../helpers/bcrypt')
+const { generateToken } = require('../helpers/jwt')
 
 class UserController {
+    static login(req, res) {
+        const { email, password } = req.body
+
+        User.findOne({
+            where: {
+                email
+            }
+        })
+        .then(user => {
+            if (!user) throw { msg: 'invalid email/password' }
+            const checkPass = comparePassword(password, user.password)
+            if (!checkPass) throw { msg: 'invalid email/password' } 
+            const access_token = generateToken({
+                id: user.id,
+                email: user.email
+            })
+            res.status(200).json({
+                access_token,
+                user
+            })
+        })
+        .catch(err => {
+            res.status(400).json(err)
+        })
+    }
+
     static register(req, res) {
         const { email, password } = req.body
         const saldo = 5000000
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,6 +2,7 @@ const userRouter = require('express').Router()
 const UserController = require('../controllers/user')
 
 userRouter.post('/register', UserController.register)
+userRouter.post('/login', UserController.login)
 
 
 module.exports = userRouter 

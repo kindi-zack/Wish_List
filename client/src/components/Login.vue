4d5b9d5e4dfdcd03d37df05482ca0fa12d998cc0
kindi-zack <alkindi.ibn.zakaria@mail.com>
2021-10-05 23:30:35 +0700

    client login form

---
+++ client/src/components/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+                <form id='loginForm' class="my-4">
+              <div class="form-group">
+                <label for="email-login">Email</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  name="email-login"
+                  id="email-login"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <label for="password-login">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  name="password-login"
+                  id="password-login"
+                  required
+                />
+              </div>
+              <button id="btn-login" type="submit" class="btn btn-dark">
+                Submit
+              </button>
+            </form>
+            <a href="#" id='show-reg'>Register</a>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/LogPage.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="login-container" class="bg-light p-3 rounded mb-5">
+        <div class="row d-flex">
+          <div class="col-md-4">
+            <h2 class="text-dark">Welcome to</h2>
+            <h2 class="text-dark">Shopping WishList</h2>
+            <p id='loginNotif' class="text-dark">Please Login first</p>
+            <p id='regNotif' class="text-dark">Please Register Your Account</p>
+
+<!-- LOGIN FORM -->
+            <Login />
+
+<!-- REGISTER -->
+            <Register />
+
+          </div>
+          <dov class="col-md-8">
+            <img class="card-img" src="../images/background_login.jpeg" alt="">
+          </dov>
+        </div>
+      </div>
+</template>
+
+<script>
+import Login from '../components/Login.vue'
+import Register from '../components/Register.vue'
+export default {
+  components: {
+    Login,
+    Register
+  }
+}
+</script>
+
+<style>
+
+</style>

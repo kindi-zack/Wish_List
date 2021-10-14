e6208ef4ad60c2ab2f7372d1baf33510aae3020f
kindi-zack <alkindi.ibn.zakaria@mail.com>
2021-10-14 15:48:50 +0700

    client logout

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,13 +1,33 @@
 <template>
       <nav class="navbar navbar-light bg-dark justify-content-between">
         <a class="navbar-brand text-white">My Wishlist</a>
-        <button id="btn-logout" class="btn btn-danger">Logout</button>
+        <button @click="logout" v-show="showLogOut" id="btn-logout" class="btn btn-danger">Logout</button>
     </nav>
 </template>
 
 <script>
 export default {
-
+  name: 'Navbar',
+  data () {
+    return {
+      // showLogOut: false
+    }
+  },
+  computed: {
+    showLogOut () {
+      return this.$store.state.showLogOut
+    }
+  },
+  methods: {
+    logout () {
+      localStorage.clear()
+      this.$router.push('/login')
+      this.$store.commit('SET_SHOWLOGOUT', false)
+    }
+  },
+  created () {
+    if (localStorage.access_token) this.$store.commit('SET_SHOWLOGOUT', true)
+  }
 }
 </script>
 
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -7,8 +7,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    showLogOut: false
   },
   mutations: {
+    SET_SHOWLOGOUT (state, value) {
+      state.showLogOut = value
+    }
   },
   actions: {
     login (context, payload) {
@@ -23,6 +27,7 @@ export default new Vuex.Store({
         .then(({ data }) => {
           console.log(data)
           localStorage.access_token = data.access_token
+          context.commit('SET_SHOWLOGOUT', true)
           router.push('/')
         })
         .catch(({ response }) => {
